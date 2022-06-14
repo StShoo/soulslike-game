a261a5fcfca55141e28d59ddda1e416ae1725cdc
StShoo <lumen.gaming777@gmail.com>
2022-06-14 16:06:40 +0300

    Changed InputHandler.cs

--- Souls_Like/Assets/Scripts/InputHandler.cs
+++ Souls_Like/Assets/Scripts/InputHandler.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+namespace SG
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InputHandler : MonoBehaviour
     {
-        
-    }
+        public float horizontal;
+        public float vertical;
+        public float moveAmount;
+        public float mouseX;
+        public float mouseY;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        PlayerControls inputActions;
+
+        Vector2 movementInput;
+        Vector2 cameraInput;
+
+        public void OnEnable()
+        {
+            if (inputActions == null)
+            {
+                inputActions = new PlayerControls();
+                inputActions.PlayerMovment.Movement.performed +=
+                    inputActions => movementInput = inputActions.ReadValue<Vector2>();
+                inputActions.PlayerMovment.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
+            }
+            
+            inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+
+        private void MoveInput(float delta)
+        {
+            horizontal = movementInput.x;
+            vertical = movementInput.y;
+            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+            mouseX = cameraInput.x;
+            mouseY = cameraInput.y;
+        }
+
+        private void TickInput(float delta)
+        { 
+            MoveInput(delta);
+        }
     }
 }
