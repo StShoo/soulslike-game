5ac2a8c9c29add07b4f49969c7d57b9b3f493956
StShoo <lumen.gaming777@gmail.com>
2022-06-14 18:25:13 +0300

    Made Camera follow Player

--- Souls_Like/Assets/Scripts/PlayerLocomotion.cs
+++ Souls_Like/Assets/Scripts/PlayerLocomotion.cs
@@ -43,6 +43,7 @@ namespace SG
             moveDirection = cameraObject.forward * inputHandler.vertical;
             moveDirection += cameraObject.right * inputHandler.horizontal;
             moveDirection.Normalize();
+            moveDirection.y = 0;
 
             float speed = movementSpeed;
             moveDirection *= speed;
