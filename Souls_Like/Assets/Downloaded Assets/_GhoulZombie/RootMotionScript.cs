5e52b8e2b3abe61f7b79c4e2a5ba689a2f85a1aa
StShoo <61821643+StShoo@users.noreply.github.com>
2023-05-11 18:48:27 +0300

    Added asset with map tiles

---
+++ Souls_Like/Assets/Downloaded Assets/_GhoulZombie/RootMotionScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+
+public class RootMotionScript : MonoBehaviour
+{
+
+    void OnAnimatorMove()
+    {
+        Animator animator = GetComponent<Animator>();
+
+        if (animator)
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.z += animator.GetFloat("Vertical") * Time.deltaTime;
+            transform.position = newPosition;
+        }
+    }
+}
