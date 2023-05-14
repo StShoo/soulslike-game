4c9147444cc34c6d952d28c0676083bee36d0cb8
StShoo <61821643+StShoo@users.noreply.github.com>
2023-05-14 18:48:52 +0300

    Created start menu and scene

---
+++ Souls_Like/Assets/Scripts/Objects/PlayGuitar.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SG;
+
+public class PlayGuitar : InteractableObject
+{
+    protected override void Interact(PlayerManager player)
+    {
+        Debug.Log("Initeate End Scene");
+    }
+}
