944511661b532ad1cf9b2ed64a2062cdd1e04167
StShoo <61821643+StShoo@users.noreply.github.com>
2023-05-14 22:35:02 +0300

    added final player model, made dark canvas

--- Souls_Like/Assets/Scripts/Objects/PlayGuitar.cs
+++ Souls_Like/Assets/Scripts/Objects/PlayGuitar.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using SG;
 
 public class PlayGuitar : InteractableObject
 {
+    [SerializeField] protected GameObject panel;
+
+    public bool isElectricBoxOn = false;
+
     protected override void Interact(PlayerManager player)
     {
-        Debug.Log("Initeate End Scene");
+
+        if (isElectricBoxOn)
+        {
+            panel.GetComponent<Animator>().SetBool("goDark", true);
+            StartCoroutine(Timedelay());
+        }
+        else
+        {
+            Debug.Log("Need to turn power on.");
+        }
+    }
+
+    protected IEnumerator Timedelay()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
